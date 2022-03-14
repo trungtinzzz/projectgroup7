c02c7d6ff6deb7065dd56b3bda0f36e63729735f
Trung Tin <votrungtinwick37@gmail.com>
2022-03-14 20:32:37 +0700

    Change style

--- projectlib.cpp
+++ projectlib.cpp
@@ -475,11 +475,11 @@ void _login(bool &isOff) {
 
     loginResult = _checkLogin(username, password);
     if (loginResult == 0) {
-        cout << "Login successfully\n" << endl;
+        cout << "Login successfully" << endl;
         staffMenu(isOff);
     }
     else if (loginResult == 1) {
-        cout << "Login successfully\n" << endl;
+        cout << "Login successfully" << endl;
         studentMenu(isOff);
     }
     else if (loginResult == -1) {
